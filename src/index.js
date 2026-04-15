41916a38c2470331343f2fa48a16e2bb2ae16dac
this-kyler-person <kylerlau11@gmail.com>
2026-04-15 21:08:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const fastify = Fastify({
 });
 fastify.register(basicAuth, {
     validate: async (username, password) => {
-        if (username !== 'kyler' || password !== 'notpassword') {
+        if (username !== 'username' || password !== 'password') {
             throw new Error('Unauthorized');
         }
     },
